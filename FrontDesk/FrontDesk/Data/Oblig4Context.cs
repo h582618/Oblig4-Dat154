35b8b241919d6dc1b23bdf01805246a3eec696be
Student <Student@U582618>
2022-04-15 15:23:28 +0200

    Frontdesk done

---
+++ FrontDesk/FrontDesk/Data/Oblig4Context.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Web_app_customer.Models;
+
+namespace Web_app_customer
+{
+    public class Oblig4Context : DbContext
+    {
+        protected override void OnConfiguring(
+             DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(
+                "Data Source=rooms4.db");
+            optionsBuilder.UseLazyLoadingProxies();
+        }
+        public DbSet<Reservation> reservations { get; set; }
+        public DbSet<Room> rooms { get; set; }
+        public DbSet<Models.Task> tasks { get; set; }
+       // public DbSet<User> users { get; set; }
+
+
+    }
+}
---
+++ FrontDesk/FrontDesk/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Web_app_customer.Models
+{
+
+    public partial class User
+    {
+        
+        public User()
+        {
+            this.Reservations = new HashSet<Reservation>();
+        }
+
+        [Key]
+        public string Username { get; set; }
+        public string Name { get; set; }
+
+        public virtual ICollection<Reservation> Reservations { get; set; }
+    }
+}
